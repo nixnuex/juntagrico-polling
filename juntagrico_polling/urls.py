2ae80eec56e882e65bf73eac33533c4865c9ec43
nixnuex <nixnuex@gmail.com>
2020-03-15 18:01:05 +0100

    Add namespace for URLs and templatetag

--- juntagrico_polling/urls.py
+++ juntagrico_polling/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from juntagrico_polling import views
 
+app_name = 'polling'
 urlpatterns = [
     path('poll/', views.poll, name='poll'),
     path('poll/vote/<int:poll_id>/<int:choice>/', views.poll, name='vote'),
